4a501c9f82f0989ef78911f98f835e79245b5c6a
Respatyadi <respatyaditu@gmail.com>
2023-02-05 12:51:54 +0700

    main

--- middleware/bcrypt.js
+++ middleware/bcrypt.js
@@ -1,5 +1,5 @@
 const bcrypt=require('bcryptjs')
-const hashedPassword = (password) => bcrypt.hashSync(password,10)
+const hashPassword = (password) => bcrypt.hashSync(password,10)
 const comparePassword = (password,hashedPassword) => bcrypt.compareSync(password,hashedPassword)
 
-module.exports = {hashedPassword,comparePassword}
+module.exports = {hashPassword,comparePassword}
