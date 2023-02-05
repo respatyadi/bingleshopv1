89d87b4a1ec587749306bbf432ca4ba6c3744953
Respatyadi <respatyaditu@gmail.com>
2023-02-05 11:36:02 +0700

    main

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const ControllerProduct = require("./controller/controllerProducts")
 const ControllerOrder = require('./controller/controllerOrders')
 const ControllerItem = require('./controller/controllerItems');
 const ControllerRole = require('./controller/controllerRoles');
-// const authentification = require('./middlewares/authen')
 const port = 3000
+
+// const authentification = require('./middlewares/authen')
+
 // const authentification = require('./middlewares/authen')
 
 // var corsOptions = {
@@ -20,21 +22,11 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
 
-app.get('/', (req,res) => {
-    res.send('Hello')
-})
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req,res) => {res.send('Bingleshopv1')})
 
 app.get('/role', ControllerRole.getRoles)
 
 app.post('/role/add', ControllerRole.addRoles)
-
-// app.get('/roles', ControllerRole.getRoles)
-
-// app.post('/roles/add', ControllerRole.addRoles)
 
 app.get('/user', ControllerUser.getUsers)
 
@@ -42,38 +34,20 @@ app.post('/user/register', ControllerUser.registerUsers)
 
 app.post('/user/login', ControllerUser.login)
 
-// app.get('/user', ControllerUser.getUsers)
-
-// app.post('/register', ControllerUser.registerUsers)
-
-// app.post('/login', ControllerUser.login)
-
 app.get('/product', ControllerProduct.getProducts)
 
 app.post('/product/register', ControllerProduct.registerProducts)
-
-// app.get('/products', ControllerProduct.getProducts)
-
-// app.post('/products/register', ControllerProduct.registerProducts)
 
 app.get('/order', ControllerOrder.getOrders)
 
 app.post('/order/add', ControllerOrder.addOrders)
 
-// app.get('/orders', ControllerOrder.getOrders)
-
-// app.post('/orders/add', ControllerOrder.addOrders)
-
 app.get('/item', ControllerItem.getItems)
 
 app.post('/item/add', ControllerItem.addItems)
 
-// app.get('/items', ControllerItem.getItems)
-
-// app.post('/items/add', ControllerItem.addItems)
-
 app.listen(port, () => {
-    console.log(`tes port bisa ${port}`)
+    console.log(`tes koneksi bisa ke port ${port}`)
 })
 
 // app.listen(port, () => {
--- middleware/jwt.js
+++ middleware/jwt.js
@@ -2,13 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const SECRET = 'secret-bingleshop-v1'
 
-// const jwt = require('jsonwebtoken')
-// const SECRET = 'secret-bingleshop'
-
 const createToken = (payload) => jwt.sign(payload,SECRET)
 const verifyToken = (token) => jwt.verify(token,SECRET)
-
-// const createToken = (payload) => jwt.sign(payload,SECRET)
-// const verifyToken = (token) => jwt.verify(token,SECRET)
 
 module.exports = {createToken,verifyToken}
